57f63b4d0729572945dd1c6e694f706017204aa3
kkosiec <32095329+kkosiec@users.noreply.github.com>
2018-01-31 16:02:50 +0100

    Chargeback non credit implemented and authorisation topology fixes (#35)

---
+++ src/main/java/com/orwellg/yggdrasil/dsl/card/transactions/chargebacknoncredit/ChargebackNonCreditTopology.java
@@ -0,0 +1,98 @@
+package com.orwellg.yggdrasil.dsl.card.transactions.chargebacknoncredit;
+
+import com.orwellg.umbrella.commons.storm.config.topology.TopologyConfig;
+import com.orwellg.umbrella.commons.storm.config.topology.TopologyConfigFactory;
+import com.orwellg.umbrella.commons.storm.topology.TopologyFactory;
+import com.orwellg.umbrella.commons.storm.topology.component.base.AbstractTopology;
+import com.orwellg.umbrella.commons.storm.topology.component.bolt.EventErrorBolt;
+import com.orwellg.umbrella.commons.storm.topology.component.bolt.KafkaEventGeneratorBolt;
+import com.orwellg.umbrella.commons.storm.topology.component.spout.KafkaSpout;
+import com.orwellg.umbrella.commons.storm.topology.generic.bolt.GBolt;
+import com.orwellg.umbrella.commons.storm.topology.generic.bolt.GRichBolt;
+import com.orwellg.umbrella.commons.storm.topology.generic.grouping.ShuffleGrouping;
+import com.orwellg.umbrella.commons.storm.topology.generic.spout.GSpout;
+import com.orwellg.umbrella.commons.storm.wrapper.kafka.KafkaBoltWrapper;
+import com.orwellg.umbrella.commons.storm.wrapper.kafka.KafkaSpoutWrapper;
+import com.orwellg.yggdrasil.dsl.card.transactions.common.bolts.EventToTransactionInfoBolt;
+import com.orwellg.yggdrasil.dsl.card.transactions.common.bolts.LoadTransactionListBolt;
+import com.orwellg.yggdrasil.dsl.card.transactions.common.bolts.ProcessNonFinancialMessageBolt;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.storm.generated.StormTopology;
+
+import java.util.Arrays;
+
+public class ChargebackNonCreditTopology extends AbstractTopology {
+
+    public static final String PROPERTIES_FILE = "chargeback-non-credit-topology.properties";
+    private static final Logger LOG = LogManager.getLogger(ChargebackNonCreditTopology.class);
+    private static final String TOPOLOGY_NAME = "dsl-card-chargeback-non-credit";
+    private static final String BOLT_NAME_PREFIX = "chargebackNonCredit";
+    private static final String KAFKA_EVENT_READER_COMPONENT = BOLT_NAME_PREFIX + "Reader";
+    private static final String MAP_EVENT = BOLT_NAME_PREFIX + "Map";
+    private static final String GET_DATA = BOLT_NAME_PREFIX + "GetData";
+    private static final String PROCESS_MESSAGE = BOLT_NAME_PREFIX + "Process";
+    private static final String EVENT_GENERATOR = BOLT_NAME_PREFIX + "EventGenerator";
+    private static final String KAFKA_EVENT_SUCCESS_PRODUCER = BOLT_NAME_PREFIX + "SuccessEventProducer";
+    private static final String ERROR_HANDLING = BOLT_NAME_PREFIX + "ErrorHandling";
+    private static final String ERROR_PRODUCER_COMPONENT = BOLT_NAME_PREFIX + "ErrorProducer";
+
+    @Override
+    public StormTopology load() {
+        // Read configuration params from the topology properties file and zookeeper
+        TopologyConfig config = TopologyConfigFactory.getTopologyConfig(PROPERTIES_FILE);
+
+        // -------------------------------------------------------
+        // Create the spout that read events from Kafka
+        // -------------------------------------------------------
+        GSpout kafkaEventReader = new GSpout(KAFKA_EVENT_READER_COMPONENT,
+                new KafkaSpoutWrapper(config.getKafkaSubscriberSpoutConfig(), String.class, String.class).getKafkaSpout(),
+                config.getKafkaSpoutHints());
+
+        // -------------------------------------------------------
+        // Process events
+        // -------------------------------------------------------
+        GBolt<?> mapBolt = new GRichBolt(MAP_EVENT, new EventToTransactionInfoBolt(),
+                config.getActionBoltHints());
+        mapBolt.addGrouping(new ShuffleGrouping(KAFKA_EVENT_READER_COMPONENT, KafkaSpout.EVENT_SUCCESS_STREAM));
+
+        GBolt<?> getDataBolt = new GRichBolt(GET_DATA, new LoadTransactionListBolt(), config.getActionBoltHints());
+        getDataBolt.addGrouping(new ShuffleGrouping(MAP_EVENT));
+
+        GBolt<?> processBolt = new GRichBolt(PROCESS_MESSAGE, new ProcessNonFinancialMessageBolt(),
+                config.getActionBoltHints());
+        processBolt.addGrouping(new ShuffleGrouping(GET_DATA));
+
+        GBolt<?> eventGeneratorBolt = new GRichBolt(EVENT_GENERATOR, new KafkaEventGeneratorBolt(), config.getActionBoltHints());
+        eventGeneratorBolt.addGrouping(new ShuffleGrouping(PROCESS_MESSAGE));
+
+        GBolt<?> kafkaEventSuccessProducer = new GRichBolt(KAFKA_EVENT_SUCCESS_PRODUCER, new KafkaBoltWrapper(config.getKafkaPublisherBoltConfig(), String.class, String.class).getKafkaBolt(), config.getEventResponseHints());
+        kafkaEventSuccessProducer.addGrouping(new ShuffleGrouping(EVENT_GENERATOR));
+
+        // -------------------------------------------------------
+
+        // -------------------------------------------------------
+        // Topology Error Handling
+        // -------------------------------------------------------
+        GBolt<?> errorHandlingBolt = new GRichBolt(ERROR_HANDLING, new EventErrorBolt(), config.getActionBoltHints());
+        errorHandlingBolt.addGrouping(new ShuffleGrouping(KAFKA_EVENT_READER_COMPONENT, KafkaSpout.EVENT_ERROR_STREAM));
+
+        GBolt<?> kafkaEventErrorProducer = new GRichBolt(ERROR_PRODUCER_COMPONENT, new KafkaBoltWrapper(config.getKafkaPublisherErrorBoltConfig(), String.class, String.class).getKafkaBolt(), config.getActionBoltHints());
+        kafkaEventErrorProducer.addGrouping(new ShuffleGrouping(ERROR_HANDLING));
+        // -------------------------------------------------------
+
+        // Topology
+        StormTopology topology = TopologyFactory.generateTopology(
+                kafkaEventReader,
+                Arrays.asList(mapBolt, getDataBolt, processBolt, eventGeneratorBolt, kafkaEventSuccessProducer, errorHandlingBolt, kafkaEventErrorProducer));
+
+        LOG.info("{} Topology created, submitting it to storm...", TOPOLOGY_NAME);
+
+        return topology;
+    }
+
+    @Override
+    public String name() {
+        return TOPOLOGY_NAME;
+    }
+}
---
+++ src/main/java/com/orwellg/yggdrasil/dsl/card/transactions/common/bolts/ProcessNonFinancialMessageBolt.java
@@ -0,0 +1,76 @@
+package com.orwellg.yggdrasil.dsl.card.transactions.common.bolts;
+
+import com.orwellg.umbrella.avro.types.gps.GpsMessageProcessed;
+import com.orwellg.umbrella.commons.storm.topology.component.bolt.BasicRichBolt;
+import com.orwellg.umbrella.commons.types.scylla.entities.cards.CardTransaction;
+import com.orwellg.umbrella.commons.types.utils.avro.DecimalTypeUtils;
+import com.orwellg.umbrella.commons.utils.enums.CardTransactionEvents;
+import com.orwellg.yggdrasil.dsl.card.transactions.model.TransactionInfo;
+import com.orwellg.yggdrasil.dsl.card.transactions.utils.GpsMessageProcessedFactory;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.storm.tuple.Tuple;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProcessNonFinancialMessageBolt extends BasicRichBolt {
+
+    private static final Logger LOG = LogManager.getLogger(ProcessNonFinancialMessageBolt.class);
+
+    @Override
+    public void declareFieldsDefinition() {
+        addFielsDefinition(Arrays.asList(
+                Fields.KEY, Fields.PROCESS_ID, Fields.EVENT_NAME, Fields.RESULT));
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        String logPrefix = null;
+        try {
+            String key = input.getStringByField(Fields.KEY);
+            String processId = input.getStringByField(Fields.PROCESS_ID);
+            TransactionInfo eventData = (TransactionInfo) input.getValueByField(Fields.EVENT_DATA);
+            List<CardTransaction> transactionList = (List<CardTransaction>) input.getValueByField(Fields.TRANSACTION_LIST);
+            logPrefix = String.format("[Key: %s][ProcessId: %s] ", key, processId);
+
+            LOG.debug("{}Processing", logPrefix);
+            GpsMessageProcessed result = GpsMessageProcessedFactory.from(eventData);
+
+            if (eventData.getSettlementAmount().compareTo(BigDecimal.ZERO) != 0) {
+                throw new IllegalArgumentException("Settlement amount cannot be non zero for a non financial transaction");
+            }
+            if (transactionList == null || transactionList.isEmpty()) {
+                throw new IllegalArgumentException("Empty transaction list - cannot process");
+            }
+            CardTransaction lastTransaction = transactionList.get(0);
+
+            result.setTotalWirecardAmount(DecimalTypeUtils.toDecimal(
+                    ObjectUtils.firstNonNull(lastTransaction.getWirecardAmount(), BigDecimal.ZERO)));
+            result.setTotalWirecardCurrency(eventData.getSettlementCurrency());
+            result.setTotalClientAmount(DecimalTypeUtils.toDecimal(
+                    ObjectUtils.firstNonNull(lastTransaction.getClientAmount(), BigDecimal.ZERO)));
+            result.setTotalClientCurrency(lastTransaction.getInternalAccountCurrency());
+            result.setTotalEarmarkAmount(DecimalTypeUtils.toDecimal(
+                    ObjectUtils.firstNonNull(lastTransaction.getEarmarkAmount(), BigDecimal.ZERO)));
+            result.setTotalEarmarkCurrency(lastTransaction.getInternalAccountCurrency());
+            result.setTotalFeesAmount(DecimalTypeUtils.toDecimal(
+                    ObjectUtils.firstNonNull(lastTransaction.getFeesAmount(), BigDecimal.ZERO)));
+            result.setTotalFeesCurrency(lastTransaction.getInternalAccountCurrency());
+
+            Map<String, Object> values = new HashMap<>();
+            values.put(Fields.KEY, key);
+            values.put(Fields.PROCESS_ID, processId);
+            values.put(Fields.EVENT_NAME, CardTransactionEvents.RESPONSE_MESSAGE.getEventName());
+            values.put(Fields.RESULT, result);
+            send(input, values);
+        } catch (Exception e) {
+            LOG.error("{}Processing error. Message: {},", logPrefix, e.getMessage(), e);
+            error(e, input);
+        }
+    }
+}
