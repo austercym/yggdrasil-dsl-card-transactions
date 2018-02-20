e8766835e79fb291ea156586feb578346735dce5
Kamil Kosiec <k.kosiec@orwellg.com>
2018-02-20 10:31:23 +0100

    Delete redundant accounting topologies

--- src/main/java/com/orwellg/yggdrasil/dsl/card/transactions/authorisationaccounting/Fields.java
+++
@@ -1,7 +0,0 @@
-package com.orwellg.yggdrasil.dsl.card.transactions.authorisationaccounting;
-
-public class Fields {
-    static final String KEY = "key";
-    static final String PROCESS_ID = "processId";
-    static final String EVENT_DATA = "eventData";
-}
